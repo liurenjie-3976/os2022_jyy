b3c0c878cbcfb72982fd845c0ed72a97874c01c9
tracer-nju <tracer@nju.edu.cn>
2023-05-04 21:51:47 +0800

    Linux jj-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:47 up  1:12,  1 user,  load average: 0.02, 0.02, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -10,6 +10,15 @@ int main(int argc, char *argv[]) {
   pid_t pid_c_p=getppid();
   printf("The current pid is: %d\n",pid_c);
   printf("The parent pid is: %d\n",pid_c_p);
+  char filename[80]="/proc/1/stat";
+  FILE *fp = fopen(filename, "r");
+  if (fp) {
+    // 用fscanf, fgets等函数读取
+    printf("fp name:%s\n",fp->_cur_column);
+    fclose(fp);
+  } else {
+    // 错误处理
+  }
 
 
   for (int i = 0; i < argc; i++) {
