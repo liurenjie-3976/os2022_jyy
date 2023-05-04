0aa9c881d2681a0b1d08add2d4476430b7dd510a
tracer-nju <tracer@nju.edu.cn>
2023-05-04 21:27:11 +0800

    Linux jj-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:11 up 48 min,  1 user,  load average: 0.10, 0.08, 0.02

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 int main(int argc, char *argv[]) {
+
+  pid_t pid_c=getpid();
+  printf("The current pid is: %d",pid_c);
+
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
